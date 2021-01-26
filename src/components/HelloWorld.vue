8057c0af0ed581b33fc4527cb4a758a8fa2c0fd0
canwdev <c2097@live.com>
2021-01-26 20:50:48 +0800

    device

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>Cordova {{ device.cordova }}</h1>
+    <h2>{{ device.manufacturer }} {{ device.model }}</h2>
+    <h2>{{ device.platform }} {{ device.version }}</h2>
+    <h2>uuid: {{ device.uuid }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      device: {}
+    }
+  },
+  mounted() {
+    document.addEventListener("deviceready", this.onDeviceReady);
+  },
+  beforeDestroy() {
+    document.removeEventListener("deviceready", this.onDeviceReady);
+  },
+  methods: {
+    onDeviceReady() {
+      console.log(device);
+      this.device = device
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: './',
-  outputDir: '../www',
+  outputDir: './www',
   productionSourceMap: false,
 }
